3c063e6752b82196ad2e0f2f85f7727a2b2e6e83
rum2mojito <wead3105@sjtu.edu.cn>
2018-08-10 10:19:59 +0800

    rotation

--- Assets/script/camera/src/camera.cs
+++ Assets/script/camera/src/camera.cs
@@ -4,13 +4,19 @@ using System.Collections;
 
 public class camera : MonoBehaviour {
 	public Transform target;
-	private Vector3 offset;
+	public float speed = 2;
+	
+	private Vector3 offset = new Vector3(0, 0, 0);
+	private Vector3 pos;
 
 	void Start() {
-		offset = target.position - this.transform.position;
+		target = GameObject.FindGameObjectWithTag("Player").transform;
 	}
 
 	void Update() {
-		this.transform.position = target.position - offset;
+		pos = target.position + offset;
+        this.transform.position = Vector3.Lerp(this.transform.position, pos, speed*Time.deltaTime);
+        Quaternion angel = Quaternion.LookRotation(target.position - this.transform.position);
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, angel, speed * Time.deltaTime);
 	}
 }
