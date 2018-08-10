a691f52634f80ac84eee65152139785cd48bb1bb
rum2mojito <wead3105@sjtu.edu.cn>
2018-08-10 21:03:33 +0800

    #4 trigger tmp

---
+++ Assets/script/trigger/tmp/chopTree.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chopTree : MonoBehaviour {
+
+	public float rayLength = 0f;
+
+	private treectrl treeScript;
+	private playerControl playerAnim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		attack();
+	}
+
+	void attack() {
+		RaycastHit hit;
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+		if(Physics.Raycast(transform.position, fwd, out hit, rayLength))
+		{
+			if(hit.collider.gameObject.tag == "Tree")
+			{
+				Debug.Log("Hit");
+				treeScript = GameObject.Find(hit.collider.gameObject.name).GetComponent<treectrl>();
+				playerAnim = GameObject.FindWithTag ("Player").GetComponent<playerControl>();
+				treeScript.treeHealth -= 1;
+			}
+		}
+	}
+}
---
+++ Assets/script/trigger/tmp/treectrl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class treectrl : MonoBehaviour {
+	public int treeHealth = 5;
+	public GameObject logs;
+	public Rigidbody tree;
+	private float speed = 8f;
+
+	// Use this for initialization
+	void Start () {
+		tree.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(treeHealth <= 0)
+		{
+			tree.isKinematic = false;
+			tree.AddForce(transform.forward * speed);
+			Wait();
+		}
+	}
+
+
+	IEnumerator Wait() {
+		Debug.Log("Before Waiting 2 seconds");
+		yield return new WaitForSeconds(7);
+		Debug.Log("After Waiting 2 Seconds");
+		DestroyTree();
+	}
+
+	void DestroyTree()
+	{
+		Destroy(tree);
+		
+		Vector3 position = new Vector3(0, 0, 0);
+		Instantiate(logs, new Vector3(2 * 2.0F, 0, 0), Quaternion.identity);
+		Instantiate(logs, tree.transform.position + new Vector3(0,0,0) + position, tree.transform.rotation);
+		Instantiate(logs, tree.transform.position + new Vector3(2,2,0) + position, tree.transform.rotation);
+		Instantiate(logs, tree.transform.position + new Vector3(5,5,0) + position, tree.transform.rotation);
+		
+	}
+}
